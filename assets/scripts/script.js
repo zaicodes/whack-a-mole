74d76a23095dc5b9860c5a63577fecd5b184cba9
Zainab <122837340+zaicodes@users.noreply.github.com>
2023-08-13 17:48:04 +0100

    Added overlay to hide the rest of the background when clicked on instructions.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,21 +2,23 @@ const instructionbtn = document.querySelector("#instructions-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 const play = document.querySelector(".play-game");
+const overlay = document.querySelector(".overlay");
 
 // modal instructions //
 function instructionlist() {
   modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
 // Close button
 function closeinstruction() {
   modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
 // Escape button function
 function closingEscape(e) {
   if (e.key === "Escape") {
     closeinstruction();
-    modal.classList.add("hidden");
   }
 }
 
