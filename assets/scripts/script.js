969aa6be57435bed7ed1ceddedb730150c5054a6
Zainab <zinabsamater@hotmail.co.uk>
2023-11-03 09:34:39 +0000

    Rearranging code and fixing comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,28 +3,32 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const allBtnSound = document.querySelector(".allbtnsound");
 const enterButton = document.querySelector("#enter-button");
+const close = document.querySelector(".close");
 
 // modal instructions //
 function instructionList() {
   allBtnSound.play();
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-} // Close button
+}
+instructionButtons.addEventListener("click", instructionList);
+
+// Close button for instructions
 function closeInstruction() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-} // Escape button function
+}
+close.addEventListener("click", closeInstruction);
+
+// Escape button function
 function closingEscape(e) {
   if (e.key === "Escape") {
     closeInstruction();
   }
 }
-
 document.addEventListener("keydown", closingEscape);
 
-const close = document.querySelector(".close");
-close.addEventListener("click", closeInstruction);
-instructionButtons.addEventListener("click", instructionList);
+// Sound for Enter button
 enterButton.addEventListener("click", () => {
   allBtnSound.play();
 });
